6c2dbb7ab9608fb204db8ec30d4934e4325426df
Selgy <imselgy@gmail.com>
2024-10-03 14:09:35 +0200

    fix

---
+++ dist/cep/js/settings/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Main from "./settings";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
